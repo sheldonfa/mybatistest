bdd1e525ff41f612731cb99a1e18daf947f05ca5
fangxin <xfang@ling-ban.com>
2017-06-19 13:23:55 +0800

    mybatis 返回map的方式

--- demo4a/src/main/java/com/Client.java
+++ demo4a/src/main/java/com/Client.java
@@ -4,20 +4,18 @@ package com;
 import com.mapper.UserMapper;
 import com.pojo.User;
 import com.resulthandler.MyMapResultHandler;
+import org.apache.ibatis.exceptions.TooManyResultsException;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.MyBatisSystemException;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.List;
 import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -30,16 +28,60 @@ public class Client {
     @Autowired
     private UserMapper userMapper;
 
+    private SqlSession getSession() throws Exception {
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        return sqlSessionFactory.openSession();
+    }
+
     /**
-     * 返回map，含有多个kv
+     * 返回Map，含有多个kv，并且自定义KV
+     * 方案：用自定义resultHandler实现的
+     *
+     * 评价：虽然实现了，但是没有用mapper的动态代理，感觉比较繁琐
      */
     @Test
     public void test1() throws Exception {
         MyMapResultHandler resultHandler = new MyMapResultHandler();
-        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBean.getObject();
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = getSession();
         sqlSession.select("com.mapper.UserMapper.findMap",resultHandler);
         Map mapResults = resultHandler.getMapResults();
         System.out.println(mapResults);
+    }
+
+    /**
+     * 如果想返回Map集合，key 自定义，value是对象，可以用selectMap方法。
+     * 参数1：方法全限定名称
+     * 参数2：指定key字段
+     *
+     * 评价：返回类型就是你的value，这个方式其实挺好了。但是也不是mapper代理的方式
+     */
+    @Test
+    public void test2() throws Exception {
+        SqlSession session = getSession();
+        Map<Integer, User> map = session.selectMap("com.mapper.UserMapper.findMap2", "id");
+        for(Map.Entry<Integer,User> entry: map.entrySet()){
+            System.out.println(entry.getKey()+"  "+ entry.getValue());
+        }
+    }
+
+    /**
+     * 下面这种写法，其实我是希望mybatis调用selectMap方法，但是它调用了selectOne
+     *
+     */
+    @Test(expected = MyBatisSystemException.class)
+    public void test3(){
+        Map<String,User> map =  userMapper.findMap3();
+        System.out.println(map);
+    }
+
+    /**
+     * 阅读源码发现，我们少了MapKey注解，所以系统不认为，我们要返回一个Map集合，反而认为我们是要返回一个User对象，这个对象的所有字段用一个Map集合装配。
+     * 为方法添加@MapKey注解，就可以使用selectMap啦
+     *
+     */
+    @Test
+    public void test4(){
+        Map<Integer, User> map4 = userMapper.findMap4();
+        System.out.println(map4);
     }
 }
--- demo4a/src/main/java/com/mapper/UserMapper.java
+++ demo4a/src/main/java/com/mapper/UserMapper.java
@@ -1,9 +1,17 @@
 package com.mapper;
 
+import com.pojo.User;
+import org.apache.ibatis.annotations.MapKey;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 
 @Repository
 public interface UserMapper {
 
+    Map<String,User> findMap3();
+
+    @MapKey(value = "id")
+    Map<Integer,User> findMap4();
 }
